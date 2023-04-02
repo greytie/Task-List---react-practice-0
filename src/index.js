7e7397f77cdf10918785634a05b9ad701055e72b
UltraFutile <ultrafutile@protonmail.com>
2023-04-02 14:19:02 -0500

    force pico dark theme

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const rootElement = document.getElementById('root');
+rootElement.setAttribute("data-theme", "dark")
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
 	<React.StrictMode>
 		<App />
